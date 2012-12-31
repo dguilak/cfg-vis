30763dab0287a2f388e2b3cd725b3cb8f46bfa0c
Daniel Guilak <daniel.guilak@gmail.com>
2012-12-31 11:18:05 -0800

    Added functionality to generate down to a specific depth.
    Still mostly hacked together, but at least I'm having fun! The
    generateToN method is quite rudimentary and can be finessed and expanded
    upon in the future. Right now, I want to explore different options for
    converting the Tree into JSON or something that d3 can read.

--- cfg.py
+++ cfg.py
@@ -1,14 +1,8 @@
 # Trying to implement a context-free grammar in Python.
-# Author: DBG
+# Using Linked List implementation currently.
+# Author: Daniel Guilak <daniel.guilak@gmail.com>
+# http://danielguilak.com/
 
-"""
-class Tree():
-    "Controls the world"
-    def __init__(self):
-        # self.root from start variable of production.
-        self.root = Node([S_])
-        self.root.__expand__()
-"""
 
 class Value():
     "A nonterminal or terminal"
@@ -29,7 +23,14 @@ eps = Value("", True)
 #Temporary production (not allowing empty string yet) -- palindrome
 production = [[a], [b], [eps], [a, S_, a], [b, S_, b]]
 
+
+
 class Node():
+    # TODO: Eventually want to keep track of when a Node is all terminals, and not call
+    # expand on it. 
+    # This is klugy and delicious, but keeps track of all leaves in the Tree.
+    leaves = []
+
     "Nodes that make up the tree"
     def __init__(self, values):
         """Constructor for Node -- should take list of values"""
@@ -37,24 +38,34 @@ class Node():
         self.children = []
         # Initial depth of tree.
         self.depth = 0
+        # Append self to leaves.
+        Node.leaves.append(self)
+        
 
     def generateToN(self, n):
-        """ Want to make it so it generates down to a specific n """
-
+        """ Want to make it so it generates down to a specific depth n """
+        # This currently is not working correctly -- don't think it's generating to a
+        # specific depth, just generating a specific number of times.
+        # For every i in n
+        for i in range(n):
+            # For every leaf, expand it!
+            # Can't use for each loop because it modifies leaves
+            for i in range(len(Node.leaves)):
+                Node.leaves[i].__expand__()
 
     def __expand__(self):
         """expand goes through values and expands the ones that are nonterminals by productions."""
         for idx, v in enumerate(self.values):
             # If we run across a nonterminal, want to replace it with the production.
             if not v.isTerminal:
+                # When you actually expand (not terminal!), you remove yourself from the leaves.
+                Node.leaves.remove(self)
                 # This will need to change with more productions.
                 # For every element in the production (i.e., everything the nonterminal can be
                 # replaced with), want to add a new node and add it to this Node's children.
                 for p in production:
                     # Temporary values; shallow copy.
                     tempValues = self.values[:]
-                    # Alex Martelli - Stack Overflow for flattening list
-                    #tempProduction = [item for sublist in production for item in sublist]
                     before = tempValues[:idx]
                     after = tempValues[idx+1:]
                     # Put it all back together again.
@@ -63,6 +74,11 @@ class Node():
 
                     # Make new node and append it to the children.
                     self.children.append(Node(before))
+
+    def __expandChildren__(self):
+        """Expand all of your children. All of them."""
+        for c in self.children:
+            c.__expand__()
 
     def __str__(self):
         returnString = ""
